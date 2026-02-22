e5dfa688b4186c5ca8f5b38c81605235a8770d7e
Преподаватель Павел Игоревич <rudnikov.paul@ya.ru>
2026-02-22 17:10:05 +0500

    Create 6.1.ino

---
+++ 6.1.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+	// Set the serial monitor baudrate to 9600
+	Serial.begin(9600);
+}
+
+void loop()
+{
+	// Variable to store ADC value ( 0 to 1023 )
+	int level;
+	// analogRead function returns the integer 10 bit integer (0 to 1023)
+	level = analogRead(0);
+
+	// Print text in serial monitor
+	Serial.println("Analog value:");
+	// Print analog value in serial monitor
+	Serial.println(level);
+}
